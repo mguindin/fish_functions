aa3d5551600fb43ece838fa30aa1499dc5b12eab
Matt Guindin <m@guind.in>
2019-04-05 15:40:26 -0400

    Updating vt to use `cfn-lint` instead of `aws cloudformation`

--- vt.fish
+++ vt.fish
@@ -1,4 +1,9 @@
-function vt -d "validate cloudformation template"
-  aws cloudformation validate-template --template-body file://$argv
+function vt --description 'validate cloudformation template using cfn-lint'
+	if not command -sq cfn-lint
+    echo "cfn-lint not found, installing"
+    # install cfn-lint
+    pip install cfn-lint
+  end
+  echo "checking $argv"
+  cfn-lint $argv --region us-east-1
 end
-
