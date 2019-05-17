1f9db9a710a36302904ddc9605885daf0578cb54
Matt Guindin <m@guind.in>
2019-05-17 15:31:00 -0400

    Fixing vt arg checking

--- vt.fish
+++ vt.fish
@@ -1,13 +1,13 @@
-function vt --description 'validate cloudformation template using cfn-lint'
+function vt --description 'validate cloudformation template using cfn-lint' --argument-names 'template'
   if not command -sq cfn-lint
     echo "cfn-lint not found, installing"
     # install cfn-lint
     pip install cfn-lint
   end
-  if not $argv
-    echo "need to pass in template to check!"
-  else
-    echo "checking $argv"
+  if test -n "$template"
+    echo "checking $template"
     cfn-lint $argv --region us-east-1
+  else
+    echo "need to pass in template to check!"
   end
 end
