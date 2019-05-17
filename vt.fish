4e9a2208b5e1c666c0cfaa949f8641bf73e31298
Matt Guindin <m@guind.in>
2019-05-17 15:24:40 -0400

    Fixing up vt function

--- vt.fish
+++ vt.fish
@@ -1,9 +1,13 @@
 function vt --description 'validate cloudformation template using cfn-lint'
-	if not command -sq cfn-lint
+  if not command -sq cfn-lint
     echo "cfn-lint not found, installing"
     # install cfn-lint
     pip install cfn-lint
   end
-  echo "checking $argv"
-  cfn-lint $argv --region us-east-1
+  if not $argv
+    echo "need to pass in template to check!"
+  else
+    echo "checking $argv"
+    cfn-lint $argv --region us-east-1
+  end
 end
